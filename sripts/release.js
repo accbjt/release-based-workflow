40fff5ff63826ac8a18a33c8ce7bf6140baa8b0e
Bill Tran <bill@billjohntran.com>
2019-06-05 13:41:21 -0700

    add push tgas

--- sripts/release.js
+++ sripts/release.js
@@ -2,10 +2,17 @@ const version = require('../package.json').version;
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function ls() {
-  const { stdout, stderr } = await exec('git status');
-  console.log('stdout:', stdout);
-  console.log('stderr:', stderr);
+async function createTag() {
+  const setTag = await exec(`git tag v${version}-${process.env.PACKAGE}`);
+
+  console.log('stdout:', setTag.stdout);
+  console.log('stderr:', setTag.stderr);
+
+  const pushTag = await exec('git push origin --tags');
+
+  console.log('stdout:' pushTag.stdout);
+  console.log('stdout:' pushTag.stderr);
 }
-ls();
+
+createTag();
 
